0d4f4ad3c6335205e12a7635ff54c5e1ce7ac794
jessica gomez <jessicajohannagomezl@gmail.com>
2024-05-09 22:16:11 -0300

    modifique cart

--- preEntrega1/src/components/Cart/Cart.jsx
+++ preEntrega1/src/components/Cart/Cart.jsx
@@ -10,59 +10,69 @@ import {
   Td,
   TableContainer,
   Button,
+  Heading,
+  Link,
 } from '@chakra-ui/react'
 import { RiDeleteBinFill } from "react-icons/ri";
+import { Link } from '@chakra-ui/react';
 
 const Cart = () => {
     const { cart, getTotal, removeItem, clearCart } = useContext(Context)
-
-
+    console.log('cart', cart );
+if (cart.lenght === 0){
+    return(
+      <Div>
+        <Heading>Todavia no agregaste productos</Heading>
+        <Link to='/'>Ver productos</Link>
+      </Div>
+    )
+}else{
   return (
-    <TableContainer>
-  <Table variant='striped' colorScheme='teal'>
-    <Thead>
-      <Tr>
-        <Th>Producto</Th>
-        <Th>Cantidad</Th>
-        <Th>Precio</Th>
-        <Th>Subtotal</Th>
-        <Th></Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      {
-        cart.map((prod) => (
-          
-          <Tr key={prod.id}>
-            <Td>{prod. nombre}</Td>
-            <Td>{prod.quantity}</Td>
-            <Td>{prod.precio}</Td>
-            <Td>{prod.precio * prod.quantity}</Td>
-            <Td> 
-              <Button onClick={() => removeItem(prod.id)}>
-                <RiDeleteBinFill />
-              </Button>
-            </Td>
-          </Tr>
-        ))
-      }
-    </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>Total: ${getTotal()}</Th>
-        <Th>
-          <Button onClick={() => clearCart()}>
-            Vaciar Carrito
-          </Button>
-        </Th>
-        <Th>
-          Finalizar Compra
-        </Th>
-      </Tr>
-    </Tfoot>
-  </Table>
-</TableContainer>
-  )
+      <TableContainer>
+    <Table variant='striped' colorScheme='teal'>
+      <Thead>
+        <Tr>
+          <Th>Producto</Th>
+          <Th>Cantidad</Th>
+          <Th>Precio</Th>
+          <Th>Subtotal</Th>
+          <Th></Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {
+          cart.map((prod) => (
+            
+            <Tr key={prod.id}>
+              <Td>{prod. nombre}</Td>
+              <Td>{prod.quantity}</Td>
+              <Td>{prod.precio}</Td>
+              <Td>{prod.precio * prod.quantity}</Td>
+              <Td> 
+                <Button onClick={() => removeItem(prod.id)}>
+                  <RiDeleteBinFill />
+                </Button>
+              </Td>
+            </Tr>
+          ))
+        }
+      </Tbody>
+      <Tfoot>
+        <Tr>
+          <Th>Total: ${getTotal()}</Th>
+          <Th>
+            <Button onClick={() => clearCart()}>
+              Vaciar Carrito
+            </Button>
+          </Th>
+          <Th>
+            Finalizar Compra
+          </Th>
+        </Tr>
+      </Tfoot>
+    </Table>
+  </TableContainer>
+    )
+  }
 }
-
 export default Cart
